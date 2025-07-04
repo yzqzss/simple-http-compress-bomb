bd3bee5d515457a528bb47a79e0d7c1d420a72d4
yzqzss <yzqzss@yandex.com>
2025-07-05 01:09:18 +0800

    f

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"compress/flate"
-	"compress/gzip"
-	"compress/lzw"
+	_ "embed"
 	"fmt"
-	"strings"
 
-	"github.com/andybalholm/brotli"
 	"github.com/gin-gonic/gin"
 )
 
-var blackHole = make([]byte, 8192*1024) // 8 MiB
-
-const maxDataSize = 20 * 1024 * 1024 * 1024 // 20G
+//go:embed boom.gz
+var boom []byte
 
 func main() {
-
 	r := gin.New()
 	// catch all panics and log them
 	// r.GET("/download", handler)
-	r.Any("/*any", handler)
+	r.Any("/*any", fileHandler)
 	err := r.Run(":8080")
 	if err != nil {
 		fmt.Println(err)
@@ -28,83 +22,14 @@ func main() {
 	}
 }
 
-func handler(c *gin.Context) {
-	// if c.Request.URL.Path == "/" {
-	c.Header("content-type", "text/html; charset=UTF-8")
-	// }
-	acceptEncodingsStr := c.GetHeader("accept-encoding")
-	acceptEncodings := strings.Split(acceptEncodingsStr, ", ")
-	if len(acceptEncodings) == 0 {
+func fileHandler(c *gin.Context) {
+	c.Header("Content-Type", "text/html; charset=UTF-8")
+	c.Header("Content-Encoding", "gzip")
+	c.Writer.WriteHeader(200)
+	_, err := c.Writer.Write(boom)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-	switch acceptEncodings[0] {
-	case "gzip":
-		fmt.Println("gzip")
-		c.Header("Content-Encoding", "gzip")
-		writer, err := gzip.NewWriterLevel(c.Writer, gzip.BestSpeed)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		totalWritten := 0
-		for totalWritten < maxDataSize {
-			n, err := writer.Write(blackHole)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			totalWritten += n
-		}
-		writer.Close()
-		return
-	case "deflate":
-		fmt.Println("deflate")
-		c.Header("Content-Encoding", "deflate")
-		writer, err := flate.NewWriter(c.Writer, flate.BestSpeed)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		totalWritten := 0
-		for totalWritten < maxDataSize {
-			n, err := writer.Write(blackHole)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			totalWritten += n
-		}
-		writer.Close()
-		return
-	case "br":
-		fmt.Println("br")
-		c.Header("Content-Encoding", "br")
-		writer := brotli.NewWriterLevel(c.Writer, brotli.BestSpeed)
-		totalWritten := 0
-		for totalWritten < maxDataSize {
-			n, err := writer.Write(blackHole)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			totalWritten += n
-		}
-		writer.Close()
-	case "compress":
-		fmt.Println("compress")
-		c.Header("Content-Encoding", "compress")
-		writer := lzw.NewWriter(c.Writer, lzw.LSB, 8)
-		totalWritten := 0
-		for totalWritten < maxDataSize {
-			n, err := writer.Write(blackHole)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			totalWritten += n
-		}
-		writer.Close()
-	default:
-		return
-	}
+	c.Writer.Flush()
 }
